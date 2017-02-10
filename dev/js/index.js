5e7993cc5907192c9d052f37da0fbc9e6c730993
Gabi Procell <gprocell927@gmail.com>
2017-02-09 17:57:01 -0700

    sets up boiler plate. user is able to call an action by clicking on a username. left off at video #10

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,23 +1,17 @@
-import 'babel-polyfill';
-import React from 'react';
-import ReactDOM from "react-dom";
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import promise from 'redux-promise';
-import createLogger from 'redux-logger';
-import allReducers from './reducers';
-import App from './components/App';
+import 'babel-polyfill'
+import React from 'react'
+import ReactDOM from "react-dom"
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import allReducers from './reducers'
+import App from './components/App'
 
-const logger = createLogger();
-const store = createStore(
-    allReducers,
-    applyMiddleware(thunk, promise, logger)
-);
+const store = createStore(allReducers)
+// /  <Provider store={store}> lets all components access the store
 
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
-);
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
---
+++ dev/js/reducers/reducer_users.js
@@ -0,0 +1,28 @@
+export default function () {
+  return [
+    {
+      id: 1,
+      first: 'Gabi',
+      last: 'Procell',
+      age: 32,
+      description: 'Gabi is a React developer and all-around awesome person',
+      thumbnail: 'https://avatars2.githubusercontent.com/u/18383962?v=3&s=460'
+    },
+    {
+      id: 2,
+      first: 'Joel',
+      last: 'Procell',
+      age: 34,
+      description: 'Joel is a MAT specialist and all-around awesome person',
+      thumbnail: 'https://s-media-cache-ak0.pinimg.com/avatars/joel_procell_1419641087_140.jpg'
+    },
+    {
+      id: 3,
+      first: 'Sophie',
+      last: 'Procell',
+      age: 6,
+      description: 'A classy lady with sass',
+      thumbnail: 'http://www.dogbreedinfo.com/images/Bluetickhannahbark.jpg'
+    }
+  ]
+}
